a07626934f8793d625af544de1b0f513b2df1047
eneskzlcn <nazifenes.kizilcin@gmail.com>
2022-01-04 17:13:26 +0300

    TestIGetTimesFromPingPostSuccessfully test case added
    In the test, I tested if I get the times from post
    request's body successfully.

--- bar_test.go
+++ bar_test.go
@@ -1,9 +1,51 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestIGetTimesFromPingPostSuccessfully(t *testing.T){
+	type Request struct{
+		Times int `json:"times"`
+	}
+	testRequest := Request{Times: 3}
+	app:= fiber.New()
 
+	app.Post("/ping", func(ctx *fiber.Ctx) error {
+		body := Request{}
+		if err := ctx.BodyParser(&body); err != nil{
+			return err
+		}
+		return ctx.JSON(body)
+	})
+
+	testRequestAsByte,err := json.Marshal(testRequest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(http.MethodPost,"/ping", strings.NewReader(string(testRequestAsByte)))
+	req.Header.Set(fiber.HeaderContentType,fiber.MIMEApplicationJSON)
+	var resp *http.Response
+	resp ,err = app.Test(req,1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body,_ := ioutil.ReadAll(resp.Body)
+	resentRequest := Request{}
+	err = json.Unmarshal(body,&resentRequest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equalf(t, resentRequest.Times,testRequest.Times,"Sended times came correctly to the api")
 }
 func TestIProducePongsAsManyAsGivenTimes(t *testing.T){
 

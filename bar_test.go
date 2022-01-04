12bf63226b564c93272e98940a69e64fd363eb65
eneskzlcn <nazifenes.kizilcin@gmail.com>
2022-01-04 16:42:19 +0300

    Project initialized with linter and go ci's
    Project initalized with an empty test file and main.go file.
    To automatize liner,build and test for go language, I added
    the workflows go.yml and super-linter.yml.

---
+++ bar_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestIGetTimesFromPingPostSuccessfully(t *testing.T){
+
+}
+func TestIProducePongsAsManyAsGivenTimes(t *testing.T){
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module barAPI
+
+go 1.17

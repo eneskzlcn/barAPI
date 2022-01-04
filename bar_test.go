6185e661bad960a994ab7c51352ad3fb1b845002
eneskzlcn <nazifenes.kizilcin@gmail.com>
2022-01-04 17:38:55 +0300

    TestIProducePongsAsManyAsGivenTimes test case is writed
    In the test, I tested is the api constructs pong array
    amount of posted times correctly.

--- bar_test.go
+++ bar_test.go
@@ -48,5 +48,44 @@ func TestIGetTimesFromPingPostSuccessfully(t *testing.T){
 	assert.Equalf(t, resentRequest.Times,testRequest.Times,"Sended times came correctly to the api")
 }
 func TestIProducePongsAsManyAsGivenTimes(t *testing.T){
+	type Request struct{
+		Times int `json:"times"`
+	}
+	testRequest := Request{Times: 3}
+	app:= fiber.New()
 
+	type PongsResponse struct{
+		Pongs []string `json:"pongs"`
+	}
+	const PONG = "pong"
+	constructPongResponseAmountOfGivenTimes := func(times int) PongsResponse{
+		var pongsResponse = PongsResponse{}
+		for i:=0 ; i<times; i++{
+			pongsResponse.Pongs = append(pongsResponse.Pongs,PONG)
+		}
+		return pongsResponse
+	}
+	app.Post("/ping", func(ctx *fiber.Ctx) error {
+		body := Request{}
+		if err := ctx.BodyParser(&body); err != nil{
+			return err
+		}
+		pongsResponse := constructPongResponseAmountOfGivenTimes(body.Times)
+		return ctx.JSON(pongsResponse)
+	})
+	testRequestAsByte,err := json.Marshal(testRequest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req := httptest.NewRequest(http.MethodPost,"/ping",strings.NewReader(string(testRequestAsByte)))
+	req.Header.Set(fiber.HeaderContentType,fiber.MIMEApplicationJSON)
+	resp,_ := app.Test(req,1)
+	body,_ := ioutil.ReadAll(resp.Body)
+
+	sentPongsResponse := PongsResponse{}
+	err = json.Unmarshal(body,&sentPongsResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equalf(t, len(sentPongsResponse.Pongs),3,"Pongs response that contains 'pong' word amount of given count")
 }
